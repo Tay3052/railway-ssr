bba00bf713a8186e5c3d35d2b5412c398eec7878
Takumi <fd3s.bcnr33@gmail.com>
2024-05-02 20:22:19 +0900

    add express

---
+++ src/server/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const port = 9000;
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
